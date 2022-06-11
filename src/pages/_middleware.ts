5f6dbf458f08b5540439bfea838e68e792d1959f
Hadi <hadibakhshi277@gmail.com>
2022-06-11 18:03:23 +0430

    middleware was removed to be checked

--- src/pages/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { setUserCookies } from "../lib/auth";
-
-export function middleware(req: NextRequest) {
-  return setUserCookies(req, NextResponse.next());
-}
